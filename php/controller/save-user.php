892be591c9f229fa94de31b27d848c2760500734
Jalen Thornton <sk8erboy042300@yahoo.com>
2015-04-30 14:34:42 -0700

    b

---
+++ php/controller/save-user.php
@@ -0,0 +1,31 @@
+<?php
+require_once(__DIR__ . "/../model/config.php");
+
+$exp = filter_(NPUT_POST, "exp", FILTER_SANITIZE_STRING); 
+$exp = filter_(NPUT_POST, "exp1", FILTER_SANITIZE_STRING); 
+$exp = filter_(NPUT_POST, "exp2", FILTER_SANITIZE_STRING); 
+$exp = filter_(NPUT_POST, "exp3", FILTER_SANITIZE_STRING); 
+$exp = filter_(NPUT_POST, "exp4", FILTER_SANITIZE_STRING); 
+
+
+$query = $_SESSION["connection"]->query("UPDATE users SET "
+            . "exp = $exp,"
+            . "exp1 = $exp1, "
+            . "exp2 = $exp2, "
+            . "exp3 = $exp3, "
+            . "exp4 = $exp4 WHERE username = \"" . $_SESSION["name"]. "\"");
+
+if($query){
+    echo "true";
+}else{
+    echo "<p>" . $_SESSION["connection"]->error . "</p> ";  
+}
+
+
+
+
+
+
+
+
+
