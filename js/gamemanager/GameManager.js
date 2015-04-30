4a88bbfb744da248fd02910711b0e3735519042e
Jalen Thornton <sk8erboy042300@yahoo.com>
2015-04-30 14:33:32 -0700

    sdjks

--- js/gamemanager/GameManager.js
+++ js/gamemanager/GameManager.js
@@ -1,19 +1,4 @@
 
-game.HeroDeathManager = Object.extend({
-    init: function(x, y, settings) {
-        this.alwaysUpdate = true;
-    },
-    update: function() {
-        if (game.data.player.dead) {
-            me.game.world.removeChild(game.data.player);
-            me.state.current().resetPlayer(10, 0);
-        }
-
-        return true;
-    }
-});
-
-
 game.ExperienceManager = Object.extend({
     init: function(x, y, settings) {
         this.alwaysUpdate = true;
@@ -23,8 +8,10 @@ game.ExperienceManager = Object.extend({
     update: function() {
         if (game.data.win === true && !this.gameOver) {
             this.gameOver(true);
+            alert("YOU WIN!");
         } else if (game.data.win === false && !this.gameOver) {
             this.gameOver(false);
+            alert("YOU LOSE!");
         }
 
         return true;
@@ -41,6 +28,33 @@ game.ExperienceManager = Object.extend({
         me.save.exp = game.data.exp;
         //for testing purposes only
         me.save.exp2 = 4;
+
+
+        $.ajax({
+            type: "POST",
+            url: "php/controller/save-user.php",
+            data: {
+                exp: game.data.exp,
+                exp1: game.data.exp1,
+                exp2: game.data.exp2,
+                exp3: game.data.exp3,
+                exp4: game.data.ex4p,
+            },
+            dataType: "text"
+        })
+                .success(function(response) {
+                    if (response === "true") {
+                        me.state.change(me.state.MENU)
+                    } else {
+                        alert(response);
+                    }
+
+                })
+                .fail(function(response) {
+                    alert("Fail");
+                });
+
+
     }
 
 });
